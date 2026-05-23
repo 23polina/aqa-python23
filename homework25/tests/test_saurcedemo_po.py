b4437241376c6d7f9321db8322df6df60b2d6efe
23polina <novik.pi23@gmail.com>
2026-05-23 19:41:14 +0200

    Added Homework25

---
+++ homework25/tests/test_saurcedemo_po.py
@@ -0,0 +1,96 @@
+import pytest
+from playwright.sync_api import expect
+from homework25.pages.login_page import Login
+from homework25.pages.swaglabs_page import SwagLabs
+from homework25.pages.bucket_page import Bucket
+from homework25.pages.checkout_page import Checkout
+from homework25.test_data.test_data import CorrectUser, InCorrectUser, ErrorMessage
+
+
+@pytest.fixture(name="login")
+def fixture_login(page):
+    return Login(page)
+
+
+@pytest.fixture(name="swaglabs")
+def fixture_swaglabs(page):
+    return SwagLabs(page)
+
+
+@pytest.fixture(name="bucket")
+def fixture_bucket(page):
+    return Bucket(page)
+
+
+@pytest.fixture(name="checkout")
+def fixture_checkout(page):
+    return Checkout(page)
+
+
+def test_user_can_logic_successfully(login, swaglabs):
+    login.goto("https://www.saucedemo.com/")
+    login.username_input.fill(CorrectUser.USERNAME)
+    login.password_input.fill(CorrectUser.PASSWORD)
+    login.login_button.click()
+    expect(swaglabs.title).to_have_text("Products")
+
+
+def test_user_cannot_logic(login):
+    login.goto("https://www.saucedemo.com/")
+    login.username_input.fill(InCorrectUser.USERNAME)
+    login.password_input.fill(InCorrectUser.PASSWORD)
+    login.login_button.click()
+    expect(login.error_message).to_have_text(ErrorMessage.INCORRECT_LOGIN)
+
+
+def test_user_adds_items_into_bucket(login, swaglabs, bucket):
+    login.goto("https://www.saucedemo.com/")
+    login.username_input.fill(CorrectUser.USERNAME)
+    login.password_input.fill(CorrectUser.PASSWORD)
+    login.login_button.click()
+    swaglabs.add_products_to_card("Sauce Labs Backpack")
+    swaglabs.add_products_to_card("Sauce Labs Bike Light")
+    swaglabs.bucket.click()
+    expect(bucket.bucket_products_validate("Sauce Labs Backpack")).to_be_visible()
+    expect(bucket.bucket_products_validate("Sauce Labs Bike Light")).to_be_visible()
+
+
+def test_user_deletes_items_from_bucket(login, swaglabs, bucket):
+    login.goto("https://www.saucedemo.com/")
+    login.username_input.fill(CorrectUser.USERNAME)
+    login.password_input.fill(CorrectUser.PASSWORD)
+    login.login_button.click()
+    swaglabs.add_products_to_card("Sauce Labs Backpack")
+    swaglabs.add_products_to_card("Sauce Labs Bike Light")
+    swaglabs.bucket.click()
+    bucket.bucket_products_deletion("Sauce Labs Backpack")
+    expect(bucket.bucket_products_validate("Sauce Labs Backpack")).not_to_be_visible()
+    expect(bucket.bucket_products_validate("Sauce Labs Bike Light")).to_be_visible()
+
+
+def test_user_cancels_checkout(login, swaglabs, bucket, checkout):
+    login.goto("https://www.saucedemo.com/")
+    login.username_input.fill(CorrectUser.USERNAME)
+    login.password_input.fill(CorrectUser.PASSWORD)
+    login.login_button.click()
+    swaglabs.add_products_to_card("Sauce Labs Backpack")
+    swaglabs.bucket.click()
+    bucket.checkout_button.click()
+    checkout.cancel_button.click()
+    expect(bucket.bucket_products_validate("Sauce Labs Backpack")).to_be_visible()
+
+
+def test_user_creates_order(login, swaglabs, bucket, checkout):
+    login.goto("https://www.saucedemo.com/")
+    login.username_input.fill(CorrectUser.USERNAME)
+    login.password_input.fill(CorrectUser.PASSWORD)
+    login.login_button.click()
+    swaglabs.add_products_to_card("Sauce Labs Backpack")
+    swaglabs.bucket.click()
+    bucket.checkout_button.click()
+    checkout.firstname_input.fill(CorrectUser.FIRSTNAME)
+    checkout.lastname_input.fill(CorrectUser.LASTNAME)
+    checkout.zipcode_input.fill(CorrectUser.ZIPCODE)
+    checkout.continue_button.click()
+    checkout.finish_button.click()
+    expect(checkout.complete_header).to_have_text("Thank you for your order!")
